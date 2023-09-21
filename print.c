98055778f9d0fb136e653bb38f683b5ac19e2f8f
Najat <craazylolla@gmail.com>
2023-09-21 14:26:20 -0700

    adding pchar

--- print.c
+++ print.c
@@ -33,3 +33,23 @@ void _pint(stack_t **h, unsigned int line_num)
 	}
 	printf("%d\n", (*h)->n);
 }
+/**
+ * _pchar - it prints the top node in stack as an ascii letter
+ * @h: the head of list
+ * @line_num: bytecode line number
+ */
+void _pchar(stack_t **h, unsigned int line_num)
+{
+	if (!h || !*h)
+	{
+		printf("L%u: can't pchar, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	if (((*h)->n) >= 0 && ((*h)->n) <= 127)
+		printf("%c\n", (*h)->n);
+	else
+	{
+		printf("L%u: can't pchar, value out of range\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+}
