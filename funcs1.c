0659ca6a52d6f2aaddad6ef49492fe91ce58f062
Najat <craazylolla@gmail.com>
2023-09-21 15:56:20 -0700

    fixes

--- funcs1.c
+++ funcs1.c
@@ -22,3 +22,22 @@ void stack_add(stack_t **new_n, __attribute__((unused))unsigned int line_n)
 	head->next = tmp;
 	tmp->prev = head;
 }
+/**
+ * stck_print - it adds a node to the stack.
+ * @stck: Pointer to a pointer pointing to top node of the stack.
+ * @line_n: line number of  the opcode.
+ */
+void stck_print(stack_t **stck, unsigned int line_n)
+{
+	stack_t *tmp;
+
+	(void) line_n;
+	if (stck == NULL)
+		exit(EXIT_FAILURE);
+	tmp = *stck;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
