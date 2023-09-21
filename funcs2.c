c75f6a00d2cca2486dac076d9702fb79d2ce624a
Najat <craazylolla@gmail.com>
2023-09-21 16:09:12 -0700

    fixes

--- funcs2.c
+++ funcs2.c
@@ -38,3 +38,23 @@ void _sub(stack_t **stck, unsigned int line_n)
 	free((*stck)->prev);
 	(*stck)->prev = NULL;
 }
+/**
+ * _div - it divides the top two elements of the stack.
+ * @stck: the top node of the stack.
+ * @line_n: the line number of of the opcode.
+ */
+void _div(stack_t **stck, unsigned int line_n)
+{
+	int sum;
+
+	if (stck == NULL || *stck == NULL || (*stck)->next == NULL)
+		_err_plus(8, line_n, "div");
+
+	if ((*stck)->n == 0)
+		_err_plus(9, line_n);
+	(*stck) = (*stck)->next;
+	sum = (*stck)->n / (*stck)->prev->n;
+	(*stck)->n = sum;
+	free((*stck)->prev);
+	(*stck)->prev = NULL;
+}
