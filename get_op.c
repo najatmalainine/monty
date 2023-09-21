1d97fc0c343d36f89dc2c8dee104f996169b1968
Najat <craazylolla@gmail.com>
2023-09-21 16:13:55 -0700

    fixes

--- get_op.c
+++ get_op.c
@@ -1,35 +1,20 @@
 #include "monty.h"
 
 /**
- * get_op - the function for selecting correct operation function
- * @tkn1: first bytecode input (opcode)
- * Return: the pointer to correct operation function
+ * _mul - it multiplies the top two elements of the stack.
+ * @stck: the top node of the stack.
+ * @line_number: the line number of of the opcode.
  */
-void (*get_op(char *tkn1))(stack_t **stack, unsigned int line_num)
+void _mul(stack_t **stck, unsigned int line_n)
 {
-	instruction_t instruction_s[] = {
-		{"pop", _pop},
-		{"pall", _pall},
-		{"pint", _pint},
-		{"swap", _swap},
-		{"add", add_},
-		{"sub", sub_},
-		{"mul", mul_},
-		{"div", div_},
-		{"mod", mod_},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"nop", _nop},
-		{"rotl", _rotl},
-		{"rotr", _rotr},
-		{NULL, NULL}};
-	int i = 0;
+	int sum;
 
-	while (instruction_s[i].f != NULL)
-	{
-		if (strcmp(tkn1, instruction_s[i].opcode) == 0)
-			return (instruction_s[i].f);
-		i++;
-	}
-	return (NULL);
+	if (stck == NULL || *stck == NULL || (*stck)->next == NULL)
+		_err_plus(8, line_n, "mul");
+
+	(*stck) = (*stck)->next;
+	sum = (*stck)->n * (*stck)->prev->n;
+	(*stck)->n = sum;
+	free((*stck)->prev);
+	(*stck)->prev = NULL;
 }
