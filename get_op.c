b619396c5db753309261bbdce22a2a7ce979db3f
Najat <craazylolla@gmail.com>
2023-09-21 16:16:55 -0700

    fixes

--- get_op.c
+++ get_op.c
@@ -18,3 +18,25 @@ void _mul(stack_t **stck, unsigned int line_n)
 	free((*stck)->prev);
 	(*stck)->prev = NULL;
 }
+/**
+ * _mod - returns the modulus of the top two elements of the stack.
+ * @stck: the top node of the stack.
+ * @line_n: the line number of of the opcode.
+ */
+void _mod(stack_t **stck, unsigned int line_n)
+{
+	int sum;
+
+	if (stck == NULL || *stck == NULL || (*stck)->next == NULL)
+
+		_err_plus(8, line_n, "mod");
+
+
+	if ((*stck)->n == 0)
+		_err_plus(9, line_n);
+	(*stck) = (*stck)->next;
+	sum = (*stck)->n % (*stck)->prev->n;
+	(*stck)->n = sum;
+	free((*stck)->prev);
+	(*stck)->prev = NULL;
+}
