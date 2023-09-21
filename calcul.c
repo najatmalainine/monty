982408ea00f74d62454dd98228d37e2c74c09c37
Najat <craazylolla@gmail.com>
2023-09-21 13:45:11 -0700

    adding mul

--- calcul.c
+++ calcul.c
@@ -33,3 +33,20 @@ void sub_(stack_t **h, unsigned int line_num)
 	free((*h)->prev);
 	(*h)->prev = NULL;
 }
+/**
+ * mul_ - multiplying second node and top node
+ * @h: top node
+ * @line_num: number of line of command
+ */
+void mul_(stack_t **h, unsigned int line_num)
+{
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't mul, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	(*h)->next->n *= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
+}
