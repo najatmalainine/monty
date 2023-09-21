5ec9bbac51feb2c65a0bd3bb8d5b51973ad8207f
Najat <craazylolla@gmail.com>
2023-09-21 15:39:58 -0700

    err

---
+++ error.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+
+/**
+ * _err - it prints the appropiate error messages
+ * determined by their error code.
+ * @error_code: The error codes
+ */
+void _err(int error_code, ...)
+{
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 1:
+			fprintf(stderr, "USAGE: monty file\n");
+			break;
+		case 2:
+			fprintf(stderr, "Error: Can't open file %s\n",
+				va_arg(ag, char *));
+			break;
+		case 3:
+			l_num = va_arg(ag, int);
+			op = va_arg(ag, char *);
+			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+			break;
+		case 4:
+			fprintf(stderr, "Error: malloc failed\n");
+			break;
+		case 5:
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+			break;
+		default:
+			break;
+	}
+	n_free();
+	exit(EXIT_FAILURE);
+}
