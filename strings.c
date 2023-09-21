be19bc5660bd3d528a96c4e504c893637d4b8b98
Najat <craazylolla@gmail.com>
2023-09-21 16:21:17 -0700

    fixes

--- strings.c
+++ strings.c
@@ -17,3 +17,30 @@ void char_print(stack_t **stck, unsigned int line_n)
 		err_str(10, line_n);
 	printf("%c\n", ascii);
 }
+/**
+ * str_print - it prints a string.
+ * @stck: the top node of the stack.
+ * @line_n: Interger representing the line number of of the opcode.
+ */
+void str_print(stack_t **stck, __attribute__((unused))unsigned int line_n)
+{
+	int ascii;
+	stack_t *temp;
+
+	if (stck == NULL || *stck == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	temp = *stck;
+	while (temp != NULL)
+	{
+		ascii = temp->n;
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		temp = temp->next;
+	}
+	printf("\n");
+}
